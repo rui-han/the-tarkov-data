a527cc9a73a1187592dde440525986ca8ba0720c
hrcn <hrcn1128@gmail.com>
2023-07-19 13:24:56 +0800

    added switch level buttons to hideout page

--- app/hideout/[hideoutId]/page.tsx
+++ app/hideout/[hideoutId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { FetchedData, HideoutParams } from "@/types/hideout";
 import { GET_HIDEOUT_DATA } from "@/graphql/queries";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
@@ -16,6 +17,7 @@ import {
   TableRow,
   TableCell,
   Table,
+  Button,
 } from "@mui/material";
 
 const styles = {
@@ -25,9 +27,6 @@ const styles = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-  },
-  cardTitle: {
-    margin: "2vh",
   },
   requirement: {
     display: "inline-flex",
@@ -48,6 +47,8 @@ export default function HideoutCard({ params }: HideoutParams) {
     (data) => data.id === params.hideoutId,
   )[0];
 
+  const [currentLevel, setCurrentLevel] = useState(1);
+
   return (
     <Grid sx={{ textAlign: "center", width: "85%" }}>
       <Box>
@@ -60,112 +61,137 @@ export default function HideoutCard({ params }: HideoutParams) {
           {hideoutData?.name}
         </Typography>
       </Box>
+      {/* switch level buttons */}
       {hideoutData?.levels.map((levelData) => (
-        <Card key={levelData.id} sx={styles.card}>
-          <CardContent>
-            <Typography variant="h4" sx={styles.cardTitle}>
-              LEVEL {levelData.level}
-            </Typography>
-            {/* Prerequisites (hideout stations) */}
-            <Typography component="div">
-              {levelData.stationLevelRequirements
-                ? levelData.stationLevelRequirements.map((preStationData) => (
-                    <Box key={preStationData.id} style={styles.requirement}>
-                      <img
-                        src={`/icons/hideout-icons/${preStationData.station.normalizedName}-icon.png`}
-                        alt=""
-                        style={styles.requirementIcon}
-                      />
-                      <span>
-                        {preStationData.station.name} lvl {preStationData.level}
-                      </span>
-                    </Box>
-                  ))
-                : null}
-            </Typography>
-            {/* Prerequisites (traders) */}
-            <Typography component="div">
-              {levelData.traderRequirements
-                ? levelData.traderRequirements.map((preTraderData) => (
-                    <Box
-                      key={preTraderData.trader.id}
-                      style={styles.requirement}
-                    >
-                      <img
-                        src={`/icons/trader-icons/${preTraderData.trader.normalizedName}-icon.jpg`}
-                        alt=""
-                        style={styles.requirementIcon}
-                      />
-                      <span>
-                        {preTraderData.trader.name} lvl {preTraderData.level}
-                      </span>
-                    </Box>
-                  ))
-                : null}
-            </Typography>
-            {/* Items table */}
-            {levelData.itemRequirements[0] ? (
-              <TableContainer sx={{ border: "0.5px solid #9a8866" }}>
-                <Table>
-                  <TableHead sx={{ backgroundColor: "#2d2d2f" }}>
-                    <TableRow>
-                      <TableCell align="center"></TableCell>
-                      <TableCell align="center">Item</TableCell>
-                      <TableCell align="center">Quantity</TableCell>
-                      <TableCell align="center">Price (24hrs Low)</TableCell>
-                      <TableCell align="center">
-                        Price (24hrs Average)
-                      </TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {levelData.itemRequirements.map((itemData) => {
-                      const itemName = itemData.item.name;
-                      const iconLink = itemData.item.iconLink;
-                      const quantity = itemData.quantity;
-                      const low24hPrice = itemData.item.low24hPrice;
-                      const avg24hPrice = itemData.item.avg24hPrice;
-                      return (
-                        <TableRow key={itemData.id}>
-                          <TableCell>
-                            <img src={iconLink} alt={itemName} />
-                          </TableCell>
-                          <TableCell align="center">{itemName}</TableCell>
-                          <TableCell align="center">x {quantity}</TableCell>
+        <Button
+          key={levelData.id}
+          onClick={() => setCurrentLevel(levelData.level)}
+          size="large"
+          color="inherit"
+          variant={levelData.level === currentLevel ? "contained" : "outlined"}
+          sx={{ mr: "1vw", mb: "2vh" }}
+        >
+          Level {levelData.level}
+        </Button>
+      ))}
+      {/* display current level requirements */}
+      {hideoutData?.levels.map((levelData) => {
+        if (levelData.level === currentLevel) {
+          return (
+            <Card key={levelData.id} sx={styles.card}>
+              <CardContent>
+                {/* Prerequisites (hideout stations) */}
+                <Typography component="div" mb="1vh">
+                  {levelData.stationLevelRequirements
+                    ? levelData.stationLevelRequirements.map(
+                        (preStationData) => (
+                          <Box
+                            key={preStationData.id}
+                            style={styles.requirement}
+                            mr="1vw"
+                          >
+                            <img
+                              src={`/icons/hideout-icons/${preStationData.station.normalizedName}-icon.png`}
+                              alt=""
+                              style={styles.requirementIcon}
+                            />
+                            <span>
+                              {preStationData.station.name} lvl{" "}
+                              {preStationData.level}
+                            </span>
+                          </Box>
+                        ),
+                      )
+                    : null}
+                </Typography>
+                {/* Prerequisites (traders) */}
+                <Typography component="div" mb="1vh">
+                  {levelData.traderRequirements
+                    ? levelData.traderRequirements.map((preTraderData) => (
+                        <Box
+                          key={preTraderData.trader.id}
+                          style={styles.requirement}
+                        >
+                          <img
+                            src={`/icons/trader-icons/${preTraderData.trader.normalizedName}-icon.jpg`}
+                            alt=""
+                            style={styles.requirementIcon}
+                          />
+                          <span>
+                            {preTraderData.trader.name} lvl{" "}
+                            {preTraderData.level}
+                          </span>
+                        </Box>
+                      ))
+                    : null}
+                </Typography>
+                {/* Items table */}
+                {levelData.itemRequirements[0] ? (
+                  <TableContainer sx={{ border: "0.5px solid #9a8866" }}>
+                    <Table>
+                      <TableHead sx={{ backgroundColor: "#2d2d2f" }}>
+                        <TableRow>
+                          <TableCell align="center"></TableCell>
+                          <TableCell align="center">Item</TableCell>
+                          <TableCell align="center">Quantity</TableCell>
                           <TableCell align="center">
-                            {low24hPrice === null || low24hPrice === 0
-                              ? "N/A"
-                              : low24hPrice}
+                            Price (24hrs Low)
                           </TableCell>
                           <TableCell align="center">
-                            {avg24hPrice === null || avg24hPrice === 0
-                              ? "N/A"
-                              : avg24hPrice}
+                            Price (24hrs Average)
                           </TableCell>
                         </TableRow>
-                      );
-                    })}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            ) : (
-              <p>No items required</p>
-            )}
-
-            {/* Construction time */}
-            <Typography sx={styles.time}>
-              Construction Time:{" "}
-              {levelData.constructionTime ? (
-                new Date(levelData.constructionTime * 1000)
-                  .toISOString()
-                  .slice(11, 19)
-              ) : (
-                <span>N/A</span>
-              )}
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
+                      </TableHead>
+                      <TableBody>
+                        {levelData.itemRequirements.map((itemData) => {
+                          const itemName = itemData.item.name;
+                          const iconLink = itemData.item.iconLink;
+                          const quantity = itemData.quantity;
+                          const low24hPrice = itemData.item.low24hPrice;
+                          const avg24hPrice = itemData.item.avg24hPrice;
+                          return (
+                            <TableRow key={itemData.id}>
+                              <TableCell>
+                                <img src={iconLink} alt={itemName} />
+                              </TableCell>
+                              <TableCell align="center">{itemName}</TableCell>
+                              <TableCell align="center">x {quantity}</TableCell>
+                              <TableCell align="center">
+                                {low24hPrice === null || low24hPrice === 0
+                                  ? "N/A"
+                                  : low24hPrice}
+                              </TableCell>
+                              <TableCell align="center">
+                                {avg24hPrice === null || avg24hPrice === 0
+                                  ? "N/A"
+                                  : avg24hPrice}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                ) : (
+                  <p>No items required</p>
+                )}
+                {/* Construction time */}
+                <Typography sx={styles.time}>
+                  Construction Time: {}
+                  {levelData.constructionTime ? (
+                    new Date(levelData.constructionTime * 1000)
+                      .toISOString()
+                      .slice(11, 19)
+                  ) : (
+                    <span>immediately</span>
+                  )}
+                </Typography>
+              </CardContent>
+            </Card>
+          );
+        }
+        return null;
+      })}
     </Grid>
   );
 }
