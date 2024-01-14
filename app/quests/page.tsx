47ed1018dcfe5365b686351e6c23dff3a2bc8f3a
Rui Han <hrcn1128@gmail.com>
2024-01-14 17:17:42 +0800

    updated quests flowchart for patch 0.14

--- app/quests/page.tsx
+++ app/quests/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import QuestImage from "../../public/images/quests.jpeg";
+import QuestImage from "../../public/images/quests.png";
 
 export default function Quests() {
   return (
