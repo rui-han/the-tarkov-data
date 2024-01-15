21a5166a1749db9a5e339eb5e84b4c95a8e80504
Rui Han <hrcn1128@gmail.com>
2024-01-15 11:34:57 +0800

    move hideout flowchart inside a popup modal

--- app/hideout/layout.tsx
+++ app/hideout/layout.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import { GET_HIDEOUT_DATA } from "@/graphql/queries";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import { FetchedData } from "@/types/hideout";
 import HideoutNav from "@/components/hideout/HideoutNav";
 import Image from "next/image";
 import HideoutFlowchart from "../../public/images/hideout-flowchart.jpeg";
+// MUI
+import { Box, Modal, Button, Divider, IconButton } from "@mui/material";
+import HighlightOffTwoToneIcon from "@mui/icons-material/HighlightOffTwoTone";
 
 export default function HideoutLayout({
   children,
@@ -16,14 +20,62 @@ export default function HideoutLayout({
 
   if (!data) return null;
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleModalOpen = () => setModalOpen(true);
+  const handleModalClose = () => setModalOpen(false);
+
   return (
     <>
       <HideoutNav hideoutStations={data.hideoutStations} />
-      <Image
-        src={HideoutFlowchart}
-        alt=""
-        style={{ width: "95%", height: "auto", marginTop: "3.5vh" }}
-      />
+      <Button
+        variant="outlined"
+        sx={{
+          m: "2vh",
+          color: "inherit",
+          borderColor: "white",
+          "&:hover": { borderColor: "red" },
+        }}
+        onClick={handleModalOpen}
+      >
+        Show Flowchart
+      </Button>
+      <Modal
+        open={modalOpen}
+        onClose={handleModalClose}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            position: "relative",
+            height: "90%",
+            width: "90%",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+          }}
+        >
+          <IconButton
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+            }}
+            onClick={handleModalClose}
+          >
+            <HighlightOffTwoToneIcon sx={{ fontSize: "2rem" }} />
+          </IconButton>
+          <Image
+            src={HideoutFlowchart}
+            alt=""
+            style={{ width: "95%", height: "auto", marginTop: "3.5vh" }}
+          />
+        </Box>
+      </Modal>
+      {/* the width of the Divider matches that of the hideout navbar Breadcrumbs, which is 90% */}
+      <Divider sx={{ color: "white", width: "90%" }} />
       {children}
     </>
   );
